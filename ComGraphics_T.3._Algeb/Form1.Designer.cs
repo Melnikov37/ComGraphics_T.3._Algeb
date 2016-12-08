51525baa3394d4072d9975b05ac2979b22d0f97a
Макс <@DESKTOP-TRJ6FQP>
2016-12-09 01:03:21 +0300

    Добавьте файлы проекта.

---
+++ ComGraphics_T.3._Algeb/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace ComGraphics_T._3._Algeb
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 14);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1000, 700);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.PictureBox1_Paint);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.button1.Location = new System.Drawing.Point(1021, 14);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(303, 700);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "т";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1333, 733);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ComGraphics_T.3._Algeb/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComGraphics_T._3._Algeb
+{
+        public partial class Form1 : Form
+        {
+            public Form1()
+            {
+                InitializeComponent();
+            }
+            private int [,] arrPix;
+
+                struct Complex
+            {
+                public double x;
+                public double y;
+
+            };
+        private static int W = 999;
+        private static int H = 699;
+        private Bitmap fractal = new Bitmap(W, H);
+        private float[] poly = new float[] { -1,0,0,0,0,1,0};
+        
+        private Complex comp(double x, double y)
+        {
+            Complex z;
+            z.x = x;
+            z.y = y;
+            return z;
+        }
+
+        private double Re(Complex z)
+        {
+            return z.x;
+        }
+
+        private double Im(Complex z)
+        {
+            return z.y;
+        }
+
+        private double mod(Complex z)
+        {
+            return Math.Sqrt(Math.Pow(z.x, 2) + Math.Pow(z.y, 2));
+        }
+
+        private Complex cadd(Complex z1, Complex z2)
+        {
+            Complex z;
+            z.x = z1.x + z2.x;
+            z.y = z1.y + z2.y;
+            return z;
+        }
+
+        private Complex csub(Complex z1, Complex z2)
+        {
+            Complex z;
+            z.x = z1.x - z2.x;
+            z.y = z1.y - z2.y;
+            return z;
+        }
+
+        private Complex crmult(double x, Complex z1)
+        {
+            Complex z;
+            z.x = z1.x * x;
+            z.y = z1.y * x;
+            return z;
+        }
+
+        private Complex cmult(Complex z1, Complex z2)
+        {
+            Complex z;
+            z.x = z1.x * z2.x - z1.y * z2.y;
+            z.y = z1.x * z2.y + z1.y * z2.x;
+            return z;
+        }
+
+
+        private Complex cdiv(Complex z1, Complex z2)
+        {
+            Complex z;
+            z.x = (z1.x * z2.x + z1.y * z2.y) / (z2.x * z2.x + z2.y * z2.y);
+            z.y = (-z1.x * z2.y + z1.y * z2.x) / (z2.x * z2.x + z2.y * z2.y);
+            return z;
+        }
+
+        private Complex f(Complex z, int deg)
+        {
+            int i;
+            Complex f;
+            f = comp(poly[deg], 0);
+            for (i = deg - 1; i >= 0; i--)
+                f = cadd(cmult(f, z), comp(poly[i], 0));
+            return f;
+        }
+        Complex df(Complex z, int deg)
+        {
+            int i;
+            Complex df;
+            df = comp(deg * poly[deg], 0);
+            for (i = deg - 1; i > 0; i--)
+                df = cadd(cmult(df, z), comp(i * poly[i], 0));
+
+            return df;
+        }
+
+        const int iter = 100000;
+            const double min = 1e-80;
+            const double max = 1e-10;
+
+
+            public void Newton_fractal()
+            {
+                arrPix = new int[W, H];
+                int n, mx, my;
+                Complex z, w = new Complex();
+                Complex fz, dfz = new Complex();
+                int deg = 5;
+                mx = W / 2;
+                my = H / 2;
+
+                for (int y = -my; y < my; y++)
+                    for (int x = -mx; x < mx; x++)
+                    {
+                        n = 0;
+                        z = comp((x) * 0.004, (y) * 0.004);
+                        w = z;
+                    for (int k = 1; k <= iter; k++)
+                    {
+                        n = k;
+                        fz = f(z, deg);
+                        dfz = df(z, deg);
+                        if (mod(dfz) <= min) { break; }
+                        w = z;
+                        z = csub(z, cdiv(fz, dfz)); 
+                        if (mod(csub(z, w)) <= max) break;
+                    }
+                    arrPix[mx + x, my + y] = n;
+                    }
+            }
+
+
+            private void PictureBox1_Paint(object sender, PaintEventArgs e)
+            {
+                Newton_fractal();
+                Pen myPen = new Pen(Color.Black, 1);
+                Graphics fid;
+                fid = Graphics.FromImage(fractal);
+                for (int i=1; i < W; i++)
+                {
+                     for (int j=1;j< H; j++)
+                     {
+                          int y = arrPix[i, j];
+                          myPen.Color = Color.FromArgb(255, (y) % 255, (y) % 255, (y*7) % 255); 
+                          e.Graphics.DrawRectangle(myPen, i, j, 1, 1);
+                     }
+                }
+           // pictureBox1.Image = fractal;
+            //fractal.Save("D://fgsfd.jpg");
+            }
+        }   
+}
